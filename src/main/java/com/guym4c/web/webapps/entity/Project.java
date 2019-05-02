f9eb6047f2fcd739f985cce36c2ce356322100d9
Guy Mac <hello@guymac.eu>
2019-05-02 23:03:17 +0100

    Add project fields

--- src/main/java/com/guym4c/web/webapps/entity/Project.java
+++ src/main/java/com/guym4c/web/webapps/entity/Project.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.guym4c.web.webapps.entity;
 
-/**
- *
- * @author hello
- */
-public class Project {
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.*;
+
+@Entity
+public class Project implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToMany
+    @JoinColumn(nullable = false)
+    private Supervisor supervisor;
+    
+    @Column(length = 199, nullable = false)
+    private String title;
+    
+    @Column(length = 999, nullable = false)
+    private String description;
+    
+    @ManyToMany
+    private Set<Topic> topics;
+    
+    @Column(nullable = false)
+    private ProjectStatus status;
+    
+    private String skills;
+    
+    public Project() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public Supervisor getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Supervisor supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(Set<Topic> topics) {
+        this.topics = topics;
+    }
+
+    public ProjectStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProjectStatus status) {
+        this.status = status;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+    
     
 }
