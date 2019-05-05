40dfc818ebc402755975a9809b9789c6b9a9b176
Guy Mac <hello@guymac.eu>
2019-05-05 22:38:50 +0100

    Add Supervisor EJB

---
+++ src/main/java/com/guym4c/web/webapps/ejb/SupervisorEJB.java
@@ -0,0 +1,22 @@
+package com.guym4c.web.webapps.ejb;
+
+import com.guym4c.web.webapps.entity.Student;
+import com.guym4c.web.webapps.entity.Supervisor;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.NOT_SUPPORTED;
+
+@Stateless
+@TransactionAttribute(NOT_SUPPORTED)
+public class SupervisorEJB extends AbstractUserEJB {
+    
+    public void create(Student student) {
+        this.em.persist(student);
+    }
+    
+    public List<Supervisor> getAll() {
+        return this.em.createNamedQuery("Supervisor.all")
+                .getResultList();
+    }
+}
