dbd0ca0b30aea1c271cc3af8dbc46655fd9bfb23
Guy Mac <hello@guymac.eu>
2019-05-05 22:38:54 +0100

    Add Topic EJB

---
+++ src/main/java/com/guym4c/web/webapps/ejb/TopicEJB.java
@@ -0,0 +1,15 @@
+package com.guym4c.web.webapps.ejb;
+
+import com.guym4c.web.webapps.entity.Topic;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.NOT_SUPPORTED;
+
+@Stateless
+@TransactionAttribute(NOT_SUPPORTED)
+public class TopicEJB extends AbstractUserEJB {
+    
+    public void create(Topic topic) {
+        this.em.persist(topic);
+    }
+}
