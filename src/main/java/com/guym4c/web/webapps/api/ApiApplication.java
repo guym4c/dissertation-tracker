cde7cbc25aeeaea43dab907c3bfc0d08d09aac94
Guy Mac <hello@guymac.eu>
2019-05-05 19:05:36 +0100

    Add REST application

---
+++ src/main/java/com/guym4c/web/webapps/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.guym4c.web.webapps.api;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApiApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>() {{
+            add(ProjectsBySupervisor.class);
+            add(StudentsBySupervisor.class);
+            add(SupervisorByStudent.class);
+        }};
+        return classes;
+    }
+}
